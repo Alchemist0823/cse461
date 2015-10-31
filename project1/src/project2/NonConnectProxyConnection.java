c156b06a3f672309c7c3dace08704c146d3d4d89
Forrest Sun <internetboy0823@gmail.com>
2015-10-31 16:00:31 -0700

    project 2 non connect

--- project1/src/project2/NonConnectProxyConnection.java
+++ project1/src/project2/NonConnectProxyConnection.java
@@ -1,8 +1,5 @@
 package project2;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 public class NonConnectProxyConnection implements Runnable {
@@ -16,16 +13,21 @@ public class NonConnectProxyConnection implements Runnable {
 
     @Override
     public void run() {
+        System.out.println("Start HTTP request");
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            OutputStream os = socket.getOutputStream();
 
-            String hostName;
+            String uri = null;
+            String hostName = null;
+            int port = -1;
             String info;
+            String header = "";
 
             while ((info = reader.readLine()) != null) {
-                System.out.println(info);
                 if (info.startsWith("GET") || info.startsWith("POST") || info.startsWith("PUT") || info.startsWith("CONNECT")) {
                     info = info.replace("HTTP/1.1", "HTTP/1.0");
+                    uri = info.split(" +")[2];
                 }
 
                 int splitPos = info.indexOf(": ");
@@ -35,7 +37,11 @@ public class NonConnectProxyConnection implements Runnable {
 
                     switch (param) {
                         case "Host":
-                            hostName = value;
+                            String[] strs = value.split(":");
+                            hostName = value.split(":")[0];
+                            if (strs.length > 1) {
+                                port = Integer.parseInt(value.split(":")[1]);
+                            }
                             break;
                         case "Connection":
                             value = "close";
@@ -43,12 +49,43 @@ public class NonConnectProxyConnection implements Runnable {
                             break;
                     }
                 }
-                System.out.println(info);
+
+                header += info + "\n\r";
+            }
+            System.out.println(header);
+
+            if (hostName != null) {
+                if (port == -1) {
+                    if (uri != null) {
+                        if (uri.startsWith("http://"))
+                            port = 80;
+                        else if (uri.startsWith("https://"))
+                            port = 443;
+                    }
+                }
+                if (port != -1) {
+                    Socket webSocket = new Socket(hostName, port);
+
+                    OutputStreamWriter writer = new OutputStreamWriter(webSocket.getOutputStream());
+                    writer.write(header);
+                    pipe(webSocket.getInputStream(), os);
+                    webSocket.close();
+                }
             }
 
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            socket.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void pipe(InputStream is, OutputStream os) throws IOException {
+        int n;
+        byte[] buffer = new byte[1024];
+        while ((n = is.read(buffer)) > -1) {
+            os.write(buffer, 0, n);   // Don't allow any extra bytes to creep in, final write
+        }
+        is.close();
     }
 }
